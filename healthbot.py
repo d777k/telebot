1870f6e7583785a2dc2c3ff635aff4300bca49b1
Egorg <egor@kasadzhik.ru>
2024-09-22 21:13:04 +0300

    addbot

---
+++ healthbot.py
@@ -0,0 +1,47 @@
+import telebot
+import sqlite3
+import os
+import csv
+
+connection = sqlite3.connect('my_database.db')
+cursor = connection.cursor()
+TOKEN = os.environ["TOKEN"]
+bot = telebot.TeleBot(TOKEN)
+def find_abbr(message):
+    try:
+        connection = sqlite3.connect('my_database.db')
+        cursor = connection.cursor()
+        rows = cursor.execute("SELECT * FROM abr WHERE name = ?",(message.text.lower(),)).fetchall()
+        return rows[0][1]
+    except:
+        bot.send_message(message.from_user.id,'Данная аббревиатура не зарегистрирована')    
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    decod_abr = find_abbr(message)
+    bot.send_message(message.from_user.id, decod_abr)    
+
+table_name = 'abr'
+file_path = 'аббревиатуры.csv'
+file_name = 'аббревиатуры.csv'
+if not(os.path.isfile(file_path)):
+    os.write(2,'Файл не найден'.encode('utf-8'))
+    quit()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS abr(
+        name TEXT NOT NULL,
+        decode TEXT NOT NULL)
+        ''')
+import fileinput
+
+for line in fileinput.input(file_name, inplace=1):
+    print(line.lower(), end='') 
+cursor.execute('DELETE FROM abr WHERE name = name ')
+file = open(file_name,encoding='utf-8')
+contents = csv.reader(file)
+insert_records = "INSERT INTO abr(name,decode) VALUES(?, ?)"
+cursor.executemany(insert_records,contents)  
+connection.commit()        
+cursor.execute('DELETE FROM abr WHERE name = name')
+bot.polling(none_stop=True, interval=5)
+connection.close()
